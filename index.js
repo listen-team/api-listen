74f70e5b2910e1ca05641be51d3f0f46061ab159
Giovanni Alexander <alexander.hh.cc@gmail.com>
2017-06-01 15:07:39 -0500

    Agregando modulo de firebase

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 'use strict';
 const express = require('express');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+const firebase = require('firebase');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-
-app.use(bodyParse.urlencoded({extended : false}));
-app.use(bodyParse.json());
-
-app.get('/:name', (req, res) => {
-	res.send({message : `Hola ${req.params.name}`});
+firebase.initializeApp({
+	serviceAccount : './database/Listen-1407cf5d699d.json',
+	databaseURL : 'https://listendb-ba317.firebaseio.com'
 });
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.get('/categoria', (req, res) => {
+	res.send(200, {categorias : []});
+});
+
+
+// para acceder a un unico recurso
+app.get('/categoria/:id', (req, res) => {
+
+});
+
+// crear categoria
+app.post('/categoria', (req, res) => {
+	let tipo = req.body.tipo;
+		const db = firebase.database();
+		let categoria = db.ref('categoria');
+
+		categoria.child('4').set({
+	 	tipo: tipo
+	});
+
+	console.log(req.body.tipo);
+	res.status(200).send({message : 'El producto ha sido recibido'});
+});
+
+// actualizar categoria
+app.put('/categoria/:id', (req, res) => {
+
+});
+
+// eliminar categoria
+app.delete('/categoria/:id', (req, res) => {
+
+});
+
+
 
 app.listen(port, () => {
 	console.log(`API REST corriendo en http://localhost:${port}`);
