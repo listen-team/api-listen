63e5b62421d31f254af48bdbbacdb7b433863fef
Giovanni Alexander <alexander.hh.cc@gmail.com>
2017-07-10 00:50:43 -0500

    Cambio

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    res.setHeader('Access-Control-Allow-Origin', 'http://exampleangular.s3-website.us-east-2.amazonaws.com:');
+    res.setHeader('Access-Control-Allow-Origin', 'http://exampleangular.s3-website.us-east-2.amazonaws.com');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
